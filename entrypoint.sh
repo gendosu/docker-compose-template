fde01d8f3a4f190486fa40d018bc9f54890e5e60
Gen Takahashi <gendosu@gmail.com>
2017-02-08 15:23:41 +0900

    entrypointの中で、Gemfile、Gemfile.lockが変更があった時のみbundleを実行するように修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-set -e
+# set -eu
 
-bundle
+#
+# check Gemfile and Gemfile.lock update
+#
+GEMFILEPREV=`cat .gemfile.sha`
+GEMFILELOCKPREV=`cat .gemfilelock.sha`
+GEMFILESHA=`shasum Gemfile | awk '{print($1)}'`
+GEMFILELOCKSHA=`shasum Gemfile.lock | awk '{print($1)}'`
 
-# rake db:migrate
-# rake db:seed
+echo "$GEMFILEPREV *Gemfile" | shasum -c -s -;
+GEMFILESTATUS=$?
+echo "$GEMFILELOCKPREV *Gemfile.lock" | shasum -c -s -;
+GEMFILELOCKSTATUS=$?
+
+if [ $GEMFILESTATUS -ne 0 -o $GEMFILELOCKSTATUS -ne 0 ] ; then
+  echo fail
+  bundle
+fi
+
+echo $GEMFILESHA > .gemfile.sha
+echo $GEMFILELOCKSHA > .gemfilelock.sha
+
+rake db:migrate
 
 if [ -e /products/tmp/pids/*.pid ]; then rm /products/tmp/pids/*.pid; fi
 
