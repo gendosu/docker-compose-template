95ae402d01c078ee697f31d5ba168575e0d1e8f2
Gen Takahashi <gendosu@gmail.com>
2017-03-06 09:46:19 +0900

    macでしか動かないコードだったので一度削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 # set -eu
 
-#
-# check Gemfile and Gemfile.lock update
-#
-GEMFILEPREV=`cat .gemfile.sha`
-GEMFILELOCKPREV=`cat .gemfilelock.sha`
-GEMFILESHA=`shasum Gemfile | awk '{print($1)}'`
-GEMFILELOCKSHA=`shasum Gemfile.lock | awk '{print($1)}'`
-
-echo "$GEMFILEPREV *Gemfile" | shasum -c -s -;
-GEMFILESTATUS=$?
-echo "$GEMFILELOCKPREV *Gemfile.lock" | shasum -c -s -;
-GEMFILELOCKSTATUS=$?
-
-if [ $GEMFILESTATUS -ne 0 -o $GEMFILELOCKSTATUS -ne 0 ] ; then
-  echo fail
-  bundle
-fi
-
-echo $GEMFILESHA > .gemfile.sha
-echo $GEMFILELOCKSHA > .gemfilelock.sha
+bundle
 
 rake db:migrate
 
